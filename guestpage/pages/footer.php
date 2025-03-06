58fd56ab988625bc5c47f28b89afcfebee2a8e46
Darek Mathias <darek.perez.vallejos@gmail.com>
2025-03-06 20:08:14 +0100

    inicial page

---
+++ guestpage/pages/footer.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="..\assets\css\footer3.css">
+
+<footer class="footer">
+    <div class="footer-links">
+        <div>
+            <a href="../pages/indexIniciado.php">Home</a>
+            <a href="#">Colaboradores</a>
+        </div>
+        <div>
+            <a href="politica_privacidad.php" target="_blank">Política de privacidad</a>
+            <a href="politica_cookies.php" target="_blank">Política de cookies</a>
+            <a href="aviso_legal.php" target="_blank">Aviso legal</a>
+            <a href="#">Buzón ético</a>
+        </div>
+        <div>
+            <a href="#">Contacto y horarios</a>
+            <a href="#">Encuéntranos en redes</a>
+            <img src="../assets/img/Isotipo.png" alt="Constellation" class="footer-image">
+
+        </div>
+    </div>
+</footer>
---
+++ guestpage/pages/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../resources/css/index.css"> 
+</head>
+<body>
+    <?php include '../pages/topnav.php';?>
+
+    <main class="main-content">
+
+        <div class="container">
+            <h1>
+                “Sky's never been the limit”
+            </h1>
+        </div>
+
+        <div class="slider-container">
+            <div class="slider">
+                <img src="assets\img\index/carousel/1.jpg" alt="Image 1">
+                <img src="assets\img\index/carousel/2.jpg" alt="Image 1">
+                <img src="assets\img\index/carousel/3.jpg" alt="Image 1">
+                <img src="assets\img\index/carousel/4.jpg" alt="Image 1">
+                <img src="assets\img\index/carousel/5.jpg" alt="Image 1">
+            </div>
+        </div>
+       
+    </main>
+
+    <div class="locationContact" id="whereAreWe">
+        <h2>Contact Us</h2>
+        <p>Feel free to reach out or visit us at our location.</p>
+
+        <div class="contact-info">
+            <p><strong>Address:</strong></p>
+            <p>Top Floor, Terminal Building</p>
+            <p>Grenier Drive, Archerfield Airport</p>
+            <p>Brisbane, Qld 4108</p>
+
+            <p> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6642.360661133741!2d153.0132940709509!3d-27.569270159697677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b914f8d160c2cad%3A0xf02a35bd7207060!2sArcherfield%20Airport!5e0!3m2!1ses!2ses!4v1741285004118!5m2!1ses!2ses" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> </p>
+            <br>
+            <p><strong>Phone:</strong> <a href="tel:+61732758000">(07) 3275 8000</a></p>
+        </div>
+    </div>
+
+</body>
+</html>
