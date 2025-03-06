66040b13b23a931ed99ac36b30376258575d034b
Darek Mathias <darek.perez.vallejos@gmail.com>
2025-03-06 20:06:53 +0100

    topnav GuestPage

---
+++ guestpage/pages/topnav.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../resources/css/topnav.css"> <!--change to relative path in order to modify design-->
+</head>
+<body>
+    <nav class="topnav">
+        <div class="logo">
+            <a href="index.php">
+                <img src="..\resources\img\redstarAW.png" alt="Logo">
+            </a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="flight-schools.php">Flight Schools</a></li>
+            <li><a href="#whereAreWe">Where are we?</a></li>
+            <li><a href="events.php">Events</a></li>
+            <li><a href="fleet.php">Fleet</a></li>
+            <li><a href="work.php">Work for Us!</a></li>
+        </ul>
+        <div class="login-btn">
+            <a href="login.php">Log in</a>
+        </div>
+    </nav>
+</body>
+</html>
